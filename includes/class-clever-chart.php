bf1d99ed7a58590d9d0ce5e3edec211da36d6ea3
Tom Donohue <foxun@hotmail.com>
2016-03-13 22:15:11 +0000

    Defining shortcode with hardcoded data

--- includes/class-clever-chart.php
+++ includes/class-clever-chart.php
@@ -75,6 +75,7 @@ class Clever_Chart {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
+		$this->define_shortcodes();
 
 	}
 
@@ -171,6 +172,22 @@ class Clever_Chart {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
+
+
+	/**
+	 * Register all of the shortcodes of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_shortcodes() {
+
+		$plugin_public = new Clever_Chart_Public( $this->get_plugin_name(), $this->get_version() ); 
+
+		$this->loader->add_shortcode( 'ccsstack', $plugin_public, 'shortcode_single_stack');
+		
+	}
+
 
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
--- public/class-clever-chart-public.php
+++ public/class-clever-chart-public.php
@@ -99,5 +99,96 @@ class Clever_Chart_Public {
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/clever-chart-public.js', array( 'jquery' ), $this->version, false );
 
 	}
+	
+	/**
+	 * A single series stacked bar chart.
+	 *
+	 * @since    1.0.0
+	 */
+	public function shortcode_single_stack($atts) {
+
+        extract(shortcode_atts(array(
+			'data' => $data,
+			'prefix' => $prefix,
+			'suffix' => $suffix), $atts));
+			
+            "category" => "",
+            "num"      => 5
+            ), $atts));
+        $cat = strtolower($category);
+        $args = array(
+            'posts_per_page'   => $num,
+            'post_type'        => 'post',
+            'post_status'      => 'publish',
+            'cat'              => $cat);
+
+		
+		$colours = array('#072A24', '#748F70', '#B0947F', '#C39D9F', '#E2CAED');
+		$colours = array('#E2C044', '#9FB1BC', '#6E8898', '#2E5266', '#D3D0CB'); //heritage
+		
+		$data = array('BP', '2.5', 'Shell', '2.2', 'Starbucks', '1.8', 'Facebook', '0.9', 'WHSmith', '0.4');
+		$prefix = "&pound;";
+		$suffix = "bn";
+		
+		if (count($data) % 2 != 0)
+			die('Not an even number of data points');
+		
+		/* First calculate the total */
+		$total = 0;
+		$numbers = array();
+		for ($i = 0; $i < count($data); $i++) {
+			if ($i % 2 == 0) {
+				$labels[] = $data[$i]; //add label to labels array
+			} else {
+				$floatval = floatval($data[$i]); //parse the number to a float
+				$total += $floatval; //add to total
+				$numbers[] = $floatval;
+			}
+			
+			// TODO $points[] = ...
+			// TODO $results[] = ...
+		}
+		
+		if ($total == 0)
+			die('Total is zero');
+		
+		/* Now build up the output array */
+		for ($i = 0; $i < count($numbers); $i++) {
+			$points[] = ($numbers[$i] / $total) * 100; //percentage width
+			$points[] = $colours[$i]; //colour
+			$points[] = $labels[$i]; //label
+			$points[] = $prefix . $numbers[$i] . $suffix; //numeric value
+		}
+		
+		$template = '
+			<figure class="clever-chart-container">
+				<div class="clever-chart-single-stack">
+					<div class="clever-chart-point" style="width: %1$s%%; background-color: %2$s; color: %2$s" title="TODO">
+						<div class="clever-chart-point-label" style="border-color: %2$s">%3$s</div>
+					</div>
+					<div class="clever-chart-point" style="width: %5$s%%; background-color: %6$s; color: %6$s" title="TODO">
+						<div class="clever-chart-point-label" style="border-color: %6$s">%7$s</div>
+					</div>
+					<div class="clever-chart-point" style="width: %9$s%%; background-color: %10$s; color: %10$s" title="TODO">
+						<div class="clever-chart-point-label" style="border-color: %10$s">%11$s</div>
+					</div>
+					<div class="clever-chart-point" style="width: %13$s%%; background-color: %14$s; color: %14$s" title="TODO">
+						<div class="clever-chart-point-label" style="border-color: %14$s">%15$s</div>
+					</div>
+				</div>
+
+				<ul class="clever-chart-results clever-chart-clearfix">
+					<li><div class="clever-chart-result" style="border-color: %2$s"><span class="clever-chart-result-label" style="color: %2$s">%3$s</span> - %4$s</div></li>
+					<li><div class="clever-chart-result" style="border-color: %6$s"><span class="clever-chart-result-label" style="color: %6$s">%7$s</span> - %8$s</div></li>
+					<li><div class="clever-chart-result" style="border-color: %10$s"><span class="clever-chart-result-label" style="color: %10$s">%11$s</span> - %12$s</div></li>
+					<li><div class="clever-chart-result" style="border-color: %14$s"><span class="clever-chart-result-label" style="color: %14$s">%15$s</span> - %16$s</div></li>
+				</ul>
+			</figure>';
+			
+		$output = vsprintf($template, $points);
+		
+		return $output;
+
+	}
 
 }
